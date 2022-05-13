5e6da621836c0768df8adc355b001e672f11a82e
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2022-05-12 18:02:17 -0600

    NVM now

--- spa/app.vue
+++ spa/app.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">  
   useHead({
     title: "Atridad's Site",
-    viewport: "width=device-width, initial-scale=1, maximum-scale=1",
     charset: "utf-8",
     lang: "en",
     meta: [
